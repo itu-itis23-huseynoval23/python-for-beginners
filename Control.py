d548da3fc9eb68323df91d9e2f391dd840cf9a32
itu-itis23-huseynoval23 <huseynoval23@itu.edu.tr>
2024-04-01 12:31:40 +0300

    Control Statement file is uploaded

---
+++ Control.py
@@ -0,0 +1,14 @@
+condition = True
+name = "Ali"
+
+if condition == True:
+    print("The condition")
+    print("was True")
+elif name == "Yunus":
+    print("Hello Yunus")
+elif name == "Orcun":
+    print("Hello Orcun")
+elif name == "Fatih":
+    print("Hello Fatih")
+else:
+    print("The condition was False")
