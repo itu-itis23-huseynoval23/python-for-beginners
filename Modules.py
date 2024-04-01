48507517d0941ec4e57d90b1585a9b59506132a7
itu-itis23-huseynoval23 <huseynoval23@itu.edu.tr>
2024-04-01 16:50:29 +0300

    Modules  file is uploaded

---
+++ Modules.py
@@ -0,0 +1,17 @@
+# math for math utilities
+# re for regular expressions
+# json to work with JSON
+# datetime to work with dates
+# sqlite3 to use SQLite
+# os for Operating System utilities
+# random for random number generation
+# statistics for statistics utilities
+# requests to perform HTTP network requests
+# http to create HTTP servers
+# urllib to manage URLs
+
+import math
+math.sqrt(4)
+
+from math import sqrt
+sqrt(4)
