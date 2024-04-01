f8f7de3dcf29be24ebc23707c2fc1ac2a30d5c9d
itu-itis23-huseynoval23 <huseynoval23@itu.edu.tr>
2024-04-01 13:49:07 +0300

    Tuples file is uploaded

---
+++ Tuples.py
@@ -0,0 +1,12 @@
+# Tuples
+# this is the same with lists but one difference is we use ()
+names = ("Ali", "Yunus", "Fatih")
+
+print(names[-1])
+print(names.index("Ali"))
+
+print(len(names))
+
+print("Roger" in names)
+print(sorted(names))
+print(newTuple = names + ("Orcun", "Enis"))
